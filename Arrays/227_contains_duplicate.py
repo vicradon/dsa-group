934afc0bd398b1674cdd555b71ce19aeed01f004
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2022-07-18 13:46:37 +0100

    227 - Contains Duplicate

---
+++ Arrays/227_contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        duplicate = {}
+        
+        for num in nums:
+            if num in duplicate:
+                return True
+            duplicate[num] = 1
+        return False
