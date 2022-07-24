ef389991d3983ff6daea5d091f4980d01d9ea084
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2022-07-24 12:18:30 +0000

    chore: found more clever solution for min stack

--- Week1/Stack/MinStack.py
+++ Week1/Stack/MinStack.py
@@ -1,43 +1,20 @@
 class MinStack:
-
     def __init__(self):
-        self.values = []
-        self.min = (float("inf"), None)
+        self.values = [] # (value, min_at_value)
+        self.min = float("inf")
         
-
     def push(self, val: int) -> None:
-        if val < self.min[0]:
-            self.min = (val, len(self.values))
-        self.values.append(val)
+        if not(self.values) or val < self.values[-1][1]:
+            self.values.append((val, val))
+        else:
+            self.values.append((val, self.values[-1][1]))
         
-
-    def pop(self) -> None:
-        if self.min[1] == len(self.values) - 1:
-            minValue = float("inf"), None
-            self.values.pop()
-            
-            for i in range(len(self.values)):
-                if self.values[i] < minValue[0]:
-                    minValue = self.values[i], i
-            self.min = minValue
-                    
-            return None
-            
+    def pop(self) -> None:            
         self.values.pop()
-        
-        
+          
     def top(self) -> int:
-        return self.values[-1]
+        return self.values[-1][0]
 
-    
     def getMin(self) -> int:
-        return self.min[0]
+        return self.values[-1][1]
         
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
